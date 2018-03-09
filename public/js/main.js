d7c11252c51b9ac709b8970537ba272f87471f9e
Daniel Stoica <daniel.stoica@racebets.com>
2018-03-09 14:24:43 +0100

    Fix typo.

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     axios.get('/api/next_races').then((response) => {
 
-<<<<<<< HEAD
             ui.populateNextRace(response.data.data.races);
 
         })
@@ -21,25 +20,5 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 .catch((err) => console.log(err));
         }
     });
-=======
-            // get checked statuses
-            let checkedTypes = ui.getCheckedRaceTypes();
-
-            if (checkedTypes.length > 0) {
-
-                // get race with highest purse
-                const highestPurseRace = nextRace.getHighestPurseRace(checkedTypes, response.data.data.races);
-
-                // display highest purse race
-                ui.displayHighestPurseRace(highestPurseRace);
-
-                // display number of runners
-                ui.displayRaceData(highestPurseRace);
-
-            }
-
-        })
-        .catch((err) => console.log(err));
->>>>>>> ae30ad8202801c588b14e303945dea00f71ab7f1
 
 });
